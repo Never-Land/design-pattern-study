7b0719dc02aac5bfbe3c7b6bbc492b8b99bac494
Never-Land <1148661875@qq.com>
2022-04-17 21:36:16 +0800

    创建学习设计模式项目

---
+++ src/main/java/com/design/pattern/HelloDesignPattern.java
@@ -0,0 +1,10 @@
+package com.design.pattern;
+
+/**
+ * 欢迎来到设计模式
+ */
+public class HelloDesignPattern{
+    public static void main(String[] args){
+        System.out.println("Hello Design Pattern!!!");
+    }
+}
---
+++ src/test/java/com/design/pattern/HelloDesignPatternTest.java
@@ -0,0 +1,15 @@
+package com.design.pattern;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Demo.
+ */
+public class HelloDesignPatternTest{
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue( true );
+    }
+}
