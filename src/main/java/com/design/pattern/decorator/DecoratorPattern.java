1294ac13e6c686d7341c6faf52031cc890eec18d
Never-Land <1148661875@qq.com>
2022-04-20 23:28:16 +0800

    装饰者模式定义

---
+++ src/main/java/com/design/pattern/decorator/DecoratorPattern.java
@@ -0,0 +1,14 @@
+package com.design.pattern.decorator;
+
+/**
+ * 装饰者模式演示
+ * 实现一个咖啡订单服务，购买咖啡时，可以加入各种配料，例如：牛奶、摩卡、
+ * 豆浆、奶泡，目前店里支持提供综合、深焙、低咖啡因、浓缩四种口味咖啡，
+ * 咖啡容量支持小杯、中杯、大杯，不同容量加入配料会增加额外收费，默认不收费，
+ * 具体收费标准可以根据市场物价自行设置，可以指定需要的咖啡容量
+ */
+public class DecoratorPattern{
+    public static void main(String[] args){
+        // TODO: 2022/4/20 待实现演示
+    }
+}
