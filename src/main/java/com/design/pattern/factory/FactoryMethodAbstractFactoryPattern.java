1b69fad1ad2e81eee4a1de8b5f9938882cadec3e
Never-Land <1148661875@qq.com>
2022-04-25 23:05:02 +0800

    工厂方法、抽象工厂模式演示需求 参考：headFirst设计模式

---
+++ src/main/java/com/design/pattern/factory/FactoryMethodAbstractFactoryPattern.java
@@ -0,0 +1,13 @@
+package com.design.pattern.factory;
+
+/**
+ * 工厂方法、抽象工厂模式演示
+ * 制作不同地区口味的披萨，为了防止制作的披萨偷工减料，店铺制作披萨
+ * 必须从区域工厂取得原料制作，目前有2个地区口味比萨(纽约【New York】
+ * 和芝加哥【Chicago】)
+ */
+public class FactoryMethodAbstractFactoryPattern{
+    public static void main(String[] args){
+        // TODO: 2022/4/25 待实现演示，思考如何实践这2种模式以及 
+    }
+}
